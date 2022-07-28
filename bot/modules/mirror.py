017b9a4e39368bc1b291c1ac74c0bf410fc4ad29
Dawn India <shuvam.dawn12345@gmail.com>
2022-07-28 17:13:53 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -451,12 +451,16 @@ def _mirror(bot, message, isZip=False, extract=False, isQbit=False, isLeech=Fals
                 link = file.get_file().file_path
 
     if not is_url(link) and not is_magnet(link) and not ospath.exists(link):
-        
+
         try:
-            bot.restrict_chat_member(chat_id=message.chat.id, user_id=message.from_user.id, until_date=int(time()) + 30, permissions=ChatPermissions(can_send_messages=False))
+            if user.status not in ['creator', 'administrator']:
+                bot.restrict_chat_member(chat_id=message.chat.id, user_id=message.from_user.id, until_date=int(time()) + 30, permissions=ChatPermissions(can_send_messages=False))
+            else: 
+                msg = f"Oww <b>Admin</b> detected!\nDidn't expect it from you.\nPlease read /{BotCommands.HelpCommand} then try again."
+                return
         except Exception as e:
             print(f'[MuteUser] Error: {type(e)} {e}')
-            
+
         help_msg = "<b>You are MUTED until you learn how to use me.</b>"
         help_msg += f"\nWatch others or read /{BotCommands.HelpCommand}"
         return sendMessage(help_msg, bot, message)
