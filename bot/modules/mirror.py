4c1e2e00375441b33aa0b4a8c6707c86d2cbbe2f
Dawn India <shuvam.dawn12345@gmail.com>
2022-07-28 17:30:28 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -458,7 +458,7 @@ def _mirror(bot, message, isZip=False, extract=False, isQbit=False, isLeech=Fals
                 bot.restrict_chat_member(chat_id=message.chat.id, user_id=message.from_user.id, until_date=int(time()) + 30, permissions=ChatPermissions(can_send_messages=False))
             else: 
                 msg = f"Oww <b>Admin</b> detected!\nDidn't expect it from you.\nPlease read /{BotCommands.HelpCommand} then try again."
-                return
+                return sendMessage(msg, bot, message)
         except Exception as e:
             print(f'[MuteUser] Error: {type(e)} {e}')
 
